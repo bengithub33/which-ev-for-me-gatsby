a0b2cbfdf5f1c313b7ae5458338e1386a84a614c
bengithub33 <47863060+bengithub33@users.noreply.github.com>
2020-03-11 18:24:41 -0500

    Update index.js
    Still minimalist but somewhat livelier copy for landing page. Feel free to edit at any juncture; I very possibly use too many exclamation points.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Need help figure out which Electric Vehicle meets you neds?</p>
-    <p>You've come to the right place!</p>
-    <Link to="/quiz/">Take the quiz</Link>
+    <h1>Which EV (Electric Vehicle) is the Right One For Me?</h1>
+    <p>So many choices. It might be that there isn’t a bad one in the lot! But everyone has specific considerations like <i>length of commute</i>, <i>price</i>, <i>vehicle size</i> – let this quiz do the heavy lifting around figuring which EV might be the best option specifically for <b><u>you</u></b>!</p>
+    <Link to="/quiz/">Take the quiz!</Link>
     <br />
     <Link to="/cars-table/">Go to cars table</Link>
   </Layout>
